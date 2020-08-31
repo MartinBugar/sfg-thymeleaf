cc370375291b75db8fcce208044d54e9f762d574
Martin <martin1.bugar@gmail.com>
2020-08-31 10:17:36 +0200

    Index. html and product.html

---
+++ src/main/java/prometheus/com/sfgthymeleaf/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package prometheus.com.sfgthymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+    @RequestMapping("/product")
+    public String getProduct(){
+
+        return "product";
+    }
+}
